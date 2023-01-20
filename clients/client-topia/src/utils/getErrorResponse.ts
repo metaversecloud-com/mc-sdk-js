f83a7159398eb39630fe550b5e5af93acf3ad941
Lina <linabell@mail.com>
2023-01-20 12:31:03 -0800

    ts cleanup

--- clients/client-topia/src/utils/getErrorResponse.ts
+++ clients/client-topia/src/utils/getErrorResponse.ts
@@ -3,29 +3,27 @@
  */
 import { AxiosError } from "axios";
 
-type errorObject = { response: { data: { errors: [{ message: string }] } } };
-
 export const getErrorResponse = ({
   error,
   message = "Something went wrong. Please try again or contact support.",
 }: {
-  error?: errorObject | unknown;
+  error?: Error | AxiosError | unknown;
   message?: string;
 }) => {
-  let errorMessage = message;
-  let status: any;
-  let data: any;
-  let url: any;
+  let data = {},
+    errorMessage = message,
+    status = 500,
+    url = "unknown";
+
   if (error instanceof AxiosError) {
     errorMessage = error?.message || message;
-    status = error?.response?.status || "unknown";
-    // errorMessage && console.error(status, errorMessage, error.config?.url);
-    // error?.response?.data && console.error(error.response.data);
-    data = error?.response?.data && error.response.data;
-    url = error?.config?.url && error.config.url;
+    if (error.response) {
+      status = error.response.status;
+      data = error.response.data;
+    }
+    if (error?.config?.url) url = error.config.url;
   } else if (error instanceof Error) {
     errorMessage = error?.message || message;
-    // errorMessage && console.error(errorMessage);
   }
 
   errorMessage = `${errorMessage}. Please surround your use of the RTSDK with a try/catch block.`;
