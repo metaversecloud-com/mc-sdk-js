624aa48b0565590c1bca3a956ba3b23c8133dadd
Daniel Liebeskind <daniel.liebeskind@gmail.com>
2023-01-20 18:40:59 -0800

    Another small change

--- clients/client-topia/src/controllers/__tests__/droppedAsset.test.ts
+++ clients/client-topia/src/controllers/__tests__/droppedAsset.test.ts
@@ -76,7 +76,7 @@ describe("DroppedAsset Class", () => {
     const mediaTypeArgs = {
       ...attributes,
       audioRadius: 0,
-      audioSliderVolume: 0.3,
+      audioSliderVolume: 30,
       isVideo: true,
       mediaLink: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
       mediaName: "string",
