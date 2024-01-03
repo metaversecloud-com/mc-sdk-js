b76ecf89055e03c92ce49f5b02ed03a94a994c01
Lina <linabell@mail.com>
2024-01-03 14:31:39 -0800

    add deleteDroppedAssets to WorldFactory

--- clients/client-topia/src/factories/WorldFactory.ts
+++ clients/client-topia/src/factories/WorldFactory.ts
@@ -1,15 +1,40 @@
-import { Topia, World } from "controllers";
+import { SDKController, Topia, World } from "controllers";
 import { WorldOptionalInterface } from "interfaces";
-
-export class WorldFactory {
-  topia: Topia;
-
+import jwt from "jsonwebtoken";
+export class WorldFactory extends SDKController {
   constructor(topia: Topia) {
-    this.topia = topia;
+    super(topia);
   }
 
   create(urlSlug: string, options?: WorldOptionalInterface): World {
     return new World(this.topia, urlSlug, options);
+  }
+
+  async deleteDroppedAssets(
+    urlSlug: string,
+    droppedAssetIds: string[],
+    interactivePublicKey: string,
+    interactiveSecret: string,
+  ) {
+    const params = { droppedAssetIds, urlSlug, interactivePublicKey, interactiveSecret };
+    try {
+      const interactiveJWT = jwt.sign(interactivePublicKey, interactiveSecret);
+
+      const promiseArray = [];
+      for (const id of droppedAssetIds) {
+        promiseArray.push(
+          this.topiaPublicApi().delete(`/world/${urlSlug}/assets/${id}`, {
+            headers: { interactiveJWT, publickey: interactivePublicKey },
+          }),
+        );
+      }
+      const result = await Promise.all(promiseArray);
+      console.log(result);
+
+      return { success: true };
+    } catch (error) {
+      throw this.errorHandler({ error, params, sdkMethod: "DroppedAssetFactory.getWithUniqueName" });
+    }
   }
 }
 
